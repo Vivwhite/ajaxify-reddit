5850b8b413bd05fd2e2d55e3941ff7b5c350a141
vivienne white <vivihansf@gmail.com>
2017-05-12 11:51:29 -0500

    added title,img and url

--- js/main.js
+++ js/main.js
@@ -1,17 +1,42 @@
 /* GLOBAL VARIABLES UP HERE */
-var frontPage = 'https://www.reddit.com/.json';
-
-
-
+var frontPage = 'https://www.reddit.com/r/aww.json';
 
 
 $(document).ready(function(){
 /* FUNCTION EXECUTION HERE */
+  getAww();
   console.log('Go forth and code!');
+
 
 });
 
-
-
 /* FUNCTION DEFINITION HERE */
+function getAww(){
+  $.ajax({
+	method: "GET",
+	url: frontPage,
+	datatype: "json",
+	success: onSuccess,
+	error: onError
+});
+}
+
+function onSuccess(json) {
+	// console.log(json.data.children.length);
+  for(i=0; i<json.data.children.length; i++){
+    var title = json.data.children[i].data.title;
+    var image = json.data.children[i].data.thumbnail;
+
+    $('.container').append(`<p>${title}</p>`);
+    $('.container').append('<img src="'+json.data.children[i].data.thumbnail+'">');
+    $('.container').append('<a href="'+json.data.children[i].data.url+'">Read More</a>')
+  }
+}
+
+function onError(xhr, status, errorThrown) {
+	alert("Sorry, there was a problem!");
+	console.log("Error: " + errorThrown);
+	console.log("Status: " + status);
+	console.dir(xhr);
+}
 /* TIP: don't forget scope! */
