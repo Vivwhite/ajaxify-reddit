105a91922eb573b1409851a8769a2db6cbb53e14
vivienne white <vivihansf@gmail.com>
2017-05-12 15:13:02 -0500

    updated styling

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /* GLOBAL VARIABLES UP HERE */
-var frontPage = 'https://www.reddit.com/r/aww.json';
+var frontPage = 'https://www.reddit.com/r/aww/.json';
 
 
 $(document).ready(function(){
@@ -28,11 +28,15 @@ function onSuccess(json) {
     var image = json.data.children[i].data.thumbnail;
     var link = json.data.children[i].data.url;
     $('#main').append(`
-                        <div class = "col-xd-6">
-                          <img class="float-left" src="${json.data.children[i].data.thumbnail}">
-                          <p class="float-right" >${title}</p>
-                          <a href="${json.data.children[i].data.url}">Read More</a><hr>
-                        </div>`);
+                      <div class="col-md-3 text-center">
+                        <div class = "card">
+                          <img class="card-img-top" src="${json.data.children[i].data.thumbnail}">
+                            <div class="card-block">
+                              <p class="float-right" >${title}</p>
+                              <button type="button" class="btn btn-info"><a href="${json.data.children[i].data.url}">Read More</a></button>
+                            </div>
+                        </div>
+                      </div>`);
   }
 }
 
